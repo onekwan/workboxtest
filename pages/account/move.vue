be205c690b40cb43204367870013a8a4587ed52d
bono <bono.sonn@gmail.com>
2024-07-12 14:52:58 +0900

    로그 추가 다시 빌드

--- pages/account/move.vue
+++ pages/account/move.vue
@@ -1,15 +1,13 @@
 <script>
 // layout null로 설정
-export default {
-	layout: "empty",
-};
+
 
 </script>
 
 <template>
 	   <div>
 	 <h1>Move</h1>
-			 <nuxt-link to="/account/move">홈으로 이동</nuxt-link>
+			 <nuxt-link to="/account/login">홈으로 이동</nuxt-link>
   </div>
 </template>
 
