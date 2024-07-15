198066e34ba00fb0b040848ae37e8c7de129c149
bono <bono.sonn@gmail.com>
2024-07-15 11:23:37 +0900

    fs 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 const { version } = require('./package.json');
+
 
 export default {
     // loading: "~/components/loading.vue",
--- static/custom-sw.js
+++ static/custom-sw.js
@@ -1,52 +1,54 @@
-const CACHE_NAME = `api-cache-${version}`; // 동적으로 설정된 캐시 이름
+
+const CACHE_NAME = 'api-cache-1.0.4';
 
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
-    );
-    self.clients.claim();
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request).then((networkResponse) => {
-                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
-                    return networkResponse;
-                }
-                const responseToCache = networkResponse.clone();
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, responseToCache).catch((error) => {
-                        console.error('Failed to cache:', error);
-                    });
-                });
-                return networkResponse;
-            });
-        }).catch((error) => {
-            console.error('Fetch failed:', error);
-            throw error;
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache).catch((error) => {
+            console.error('Failed to cache:', error);
+          });
+        });
+        return networkResponse;
+      });
+    }).catch((error) => {
+      console.error('Fetch failed:', error);
+      throw error;
+    })
+  );
 });
 
 // 업데이트 감지
 self.addEventListener('message', (event) => {
-    if (event.data === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
+        
