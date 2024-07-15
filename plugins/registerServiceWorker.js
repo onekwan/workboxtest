5201ce5c6deb87e09afad9a01104e624fb98ca95
bono <bono.sonn@gmail.com>
2024-07-15 11:53:04 +0900

    9999

--- plugins/registerServiceWorker.js
+++ plugins/registerServiceWorker.js
@@ -18,12 +18,13 @@ if ('serviceWorker' in navigator) {
                 if (installingWorker.state === 'installed') {
                     if (navigator.serviceWorker.controller) {
                         console.log('새 컨텐츠 사용 가능');
-                        if (confirm('새로운 업데이트가 있습니다. 페이지를 새로고침 하시겠습니까?')) {
-                            console.log('사용자가 새로고침 수락');
-                            window.location.reload();
-                        } else {
-                            console.log('사용자가 새로고침 거부');
-                        }
+                        window.location.reload();
+                        // if (confirm('새로운 업데이트가 있습니다. 페이지를 새로고침 하시겠습니까?')) {
+                        //     console.log('사용자가 새로고침 수락');
+                        //
+                        // } else {
+                        //     console.log('사용자가 새로고침 거부');
+                        // }
                     } else {
                         console.log('컨텐츠가 오프라인 사용을 위해 캐시됨');
                     }
